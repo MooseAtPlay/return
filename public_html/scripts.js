2a7f24cfcb6e3ad6a7b26705db0fc95e7dc51624
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-08-17 23:32:03 -0400

    elements and global variables

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,4 +1,17 @@
+function createRoom() {}
+
 function start() {
-    console.log("start up code");
+    console.log(tiles), console.log(rooms), console.log(room);
 }
+
+var tiles = {
+    W: {
+        s: [ -1, -1 ],
+        w: !1
+    },
+    o: {
+        s: [ -6, -2 ],
+        t: "Nice path!"
+    }
+}, rooms = [ "WWWWWWWWWWWWWWWooooooooooooWWooooooooooooWWooooooooooooWWooooooooooooWWooooooooooooWWooooooooooooWWooooooooooooWWWWWWWWWWWWWWW" ], D = document, byId = D.getElementById.bind(D), room = byId("room");
 //# sourceMappingURL=scripts.js.map
--- source/js/return.js
+++ source/js/return.js
@@ -1,3 +1,35 @@
+var tiles = {
+  W: { s: [ -1, -1 ], w: false },
+  o: { s: [ -6, -2 ], t: 'Nice path!' }
+};
+
+
+var rooms = [
+  // 0 - What now?
+  'WWWWWWWWWWWWWW' + 
+  'WooooooooooooW' + 
+  'WooooooooooooW' + 
+  'WooooooooooooW' + 
+  'WooooooooooooW' + 
+  'WooooooooooooW' + 
+  'WooooooooooooW' + 
+  'WooooooooooooW' + 
+  'WWWWWWWWWWWWWW' 
+];
+
+var D = document;
+var byId = D.getElementById.bind( D );
+var room = byId( 'room' );
+
+function createRoom() {
+  // create the initial room div layout
+  // #room should already exist in HTML
+  // 9 div.row elements, each having 14 (unclassed) child div elements
+}
+
 function start() {
-  console.log('start up code');  
+  console.log( tiles );
+  console.log( rooms );
+  console.log( room );
+
 }
